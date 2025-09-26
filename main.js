80270a10db3d4190ba902c7a99eeb019a78ee1e5
Halyna Sernetska <h.sernetska@rgu.ac.uk>
2025-09-26 08:19:12 +0100

    add function to start quiz

---
+++ main.js
@@ -0,0 +1,17 @@
+const prompt = require("readline-sync");
+const { startQuiz } = require("./quiz");
+const emoji = require("node-emoji");
+
+// Variables to store quiz state 
+function displayWelcome() {
+    console.log("=".repeat(50));
+    console.log("🎬 WELCOME TO THE MOVIE TRIVIA QUIZ! 🎬");
+    console.log("=".repeat(50));
+    console.log("Test your knowledge of movies and pop culture!");
+    console.log("");
+}
+function startQuizApp() {
+    displayWelcome();
+    getPlayerName();
+    showMainMenu();
+}
